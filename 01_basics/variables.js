febf5d85d870ecdb81af2697eaad1ed0865fae3c
DavidGhimire1 <124151603+DavidGhimire1@users.noreply.github.com>
2024-06-18 10:09:40 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 15155
+let accountEmail = "david@gmail.com"
+var accountPassword = "123456"
+accountCity = "Kathmandu"
+let accountState
+
+// accountId = 2 
+accountEmail = "h@gmail.com"
+accountPassword = "123"
+accountCity = "Pokhara"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
