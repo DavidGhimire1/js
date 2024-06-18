254f7222525dc44f291ee21604f4211d4fc74ce1
DavidGhimire1 <124151603+DavidGhimire1@users.noreply.github.com>
2024-06-18 10:26:15 +0000

    converstion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "54"
+
+//console.log(typeof score);
+
+let valueIdNumber = Number(score)
+//console.log(typeof valueIdNumber);
+
+// "33" => 33
+// "54abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "david" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
